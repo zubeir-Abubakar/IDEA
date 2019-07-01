b7a67b6b1b965df508a6bc0927e6c0e0d1218f95
zubeir-Abubakar <zubkayare@gmail.com>
2019-07-01 13:02:17 +0300

    vote

--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Pitch = Pitch, Category = Category, Vote = Vote, Comment = Comment)
+    return dict(app = app,db = db,User = User, Pitch = Pitch, Category = Category, V  ote = Vote, Comment = Comment)
 
 
 if __name__ == '__main__':
